3bcb1ca984cd1bbc973aaa4e8fc4b23f2379bcd4
Guilherme Ferreira Silva <guilhermeferreira7930@gmail.com>
2025-04-12 15:32:19 -0300

    codigo com bug na parte testes (CI)

--- src/test/java/br/com/fiap/coletalixo/ColetalixoApplicationTests.java
+++ src/test/java/br/com/fiap/coletalixo/ColetalixoApplicationTests.java
@@ -3,11 +3,14 @@ package br.com.fiap.coletalixo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.fail;
+
 @SpringBootTest
 class ColetalixoApplicationTests {
 
 	@Test
 	void contextLoads() {
+		fail("Teste CI com erro no test");
 	}
 
 }
