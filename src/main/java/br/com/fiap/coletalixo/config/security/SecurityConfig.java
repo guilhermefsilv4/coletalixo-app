d8de7204a281b750b309201ef455440839267d38
Guilherme Ferreira Silva <guilhermeferreira7930@gmail.com>
2025-04-12 18:26:20 -0300

    mais uma tentativa de liberar o auth

--- src/main/java/br/com/fiap/coletalixo/config/security/SecurityConfig.java
+++ src/main/java/br/com/fiap/coletalixo/config/security/SecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -79,5 +80,10 @@ public class SecurityConfig {
                         .allowedHeaders("*");
             }
         };
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers("/auth/**");
     }
 }
