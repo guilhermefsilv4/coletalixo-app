890610803f6d7a73370385fbe9b475e98fe31280
Guilherme Ferreira Silva <guilhermeferreira7930@gmail.com>
2025-05-05 21:31:48 -0300

    removendo anotacao

--- src/test/java/br/com/fiap/coletalixo/config/CucumberSpringConfiguration.java
+++ src/test/java/br/com/fiap/coletalixo/config/CucumberSpringConfiguration.java
@@ -4,7 +4,6 @@ import io.cucumber.spring.CucumberContextConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @CucumberContextConfiguration
-@SpringBootTest
 public class CucumberSpringConfiguration {
     // Classe vazia, apenas para configuração
 } 
